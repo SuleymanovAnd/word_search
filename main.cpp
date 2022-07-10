c38b3d50778768124894cfbd1ef3591b7e6aed34
Suleymanov Andrey <sasus37@gmail.com>
2022-07-10 15:11:50 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+   std::ifstream words;
+   words.open ("E:\\SkillboxProject\\words.txt");
+
+   std::string searchWord;
+   std::cout << "enter the search word: ";
+   std::cin >> searchWord;
+   int  countWord = 0;
+   while (!words.eof()){
+       std::string temp;
+       words >> temp;
+      if (temp == searchWord) {countWord++;}
+   }
+   std::cout << countWord;
 }
